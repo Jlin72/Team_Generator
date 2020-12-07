fac91a6b720a5d02ab13a2006f5a9e1c3b475fb1
Jhonny Lin <jhonnylin72@gmail.com>
2020-12-06 21:58:28 -0500

    Fixed the files inside the template folder

--- app.js
+++ app.js
@@ -10,31 +10,8 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// Empty array name team. This empty array will be used to contain the answers from inquierer. Which will then be used to generate the HTML file.
 const team = [];
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
 
 function teamCreate () {
 
@@ -47,7 +24,7 @@ function teamCreate () {
                     name:'name',
                     message:"Name of manager",
                     validate: function (answer) {
-                        if(answer === "" || /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(answer) === true || /\d/.test(answer) === true) {
+                        if(answer === "" || /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(answer) === true || /\d/.test(answer) === true) {
                             return "Please enter a name for the manager, no special characters, or numbers. If you see this error correct the last mistake and press enter when done";
                         } return true;
                     }
@@ -57,8 +34,8 @@ function teamCreate () {
                     name: "id",
                     message:"Enter an id for the manager",
                     validate: function (answer) {
-                        if (answer === "") {
-                            return"Please enter an ID";
+                        if (answer === "" || answer.length < 4) {
+                            return"Please enter an ID, id must be longer than 4 letters/digits";
                         } return true;
                     }
                 },
@@ -100,7 +77,7 @@ function teamCreate () {
                 name: "name",
                 message:"Enter the name of your engineer",
                 validate: function (answer) {
-                    if(answer === "" || /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(answer) === true || /\d/.test(answer) === true) {
+                    if(answer === "" || /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(answer) === true || /\d/.test(answer) === true) {
                         return "Please enter a name for the manager, no special characters, or numbers. If you see this error correct the last mistake and press enter when done";
                     } return true;
                 }
@@ -204,7 +181,7 @@ function teamCreate () {
                     type: "list",
                     name: "moremembers",
                     message: "Please select which team members do you want to add? Select none if you don't want to keep adding members",
-                    choices: ["Engineer", "Intern", "None"]
+                    choices: ["Engineer", "Intern", "No more team members to add"]
                 }
             ])
             .then(answer => {
@@ -215,9 +192,9 @@ function teamCreate () {
                     case "Intern":
                         createIntern();
                         break;
-                    case "None":
+                    case "No more team members to add":
                         fs.writeFile(outputPath,render(team), err => {
-                            err?console.log(err):console.log("success");
+                            err?console.log(err):console.log("Success, team.html has been created.");
                         })
                         break;
                 }
