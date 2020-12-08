564c5853f3b7f0f4d6a4a229d53ae06c1ca4cf02
Jhonny Lin <jhonnylin72@gmail.com>
2020-12-07 19:13:02 -0500

    Fixed app.js intern input name validator

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ function teamCreate () {
                 name: "name",
                 message:"Enter the name of your intern.",
                 validate: function (answer) {
-                    if(answer === "" || /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(answer) === true || /\d/.test(answer) === true) {
+                    if(answer === "" || /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(answer) === true || /\d/.test(answer) === true) {
                         return "Please enter a name for the manager, no special characters, or numbers. If you see this error correct the last mistake and press enter when done";
                     } return true;
                 }
